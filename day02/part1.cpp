4ce3f8ddda4902daf3a071b4d073b7bc579af5d8
Jez Swanson <swansonjeremy@google.com>
2025-12-04 13:22:59 -0800

    Initial commit of boilerplate for advent of code. Thx Gemini

---
+++ day02/part1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <input_file>" << std::endl;
+        return 1;
+    }
+
+    std::ifstream input(argv[1]);
+    if (!input.is_open()) {
+        std::cerr << "Could not open input file " << argv[1] << std::endl;
+        return 1;
+    }
+    
+    std::cout << "Solving Day 02 Part 1..." << std::endl;
+
+    std::string line;
+    while (std::getline(input, line)) {
+        // Part 1 logic here
+    }
+
+    input.close();
+    return 0;
+}
