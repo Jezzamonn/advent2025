bc0467632152dc2f1f35309f1261e9a432852453
Jez Swanson <swansonjeremy@google.com>
2025-12-08 13:54:01 -0800

    Day 2, part 1

--- day02/part1.cpp
+++ day02/part1.cpp
@@ -3,6 +3,46 @@
 #include <string>
 #include <vector>
 
+
+// Lazy but maybe fast way of doing this?
+bool isBadId(uint64_t num) {
+    if (num < 10) {
+        return false;
+    }
+    if (num < 100) {
+        return num % 11 == 0;
+    }
+    if (num < 1000) {
+        return false;
+    }
+    if (num < 10000) {
+        return num % 101 == 0;
+    }
+    if (num < 100000) {
+        return false;
+    }
+    if (num < 1000000) {
+        return num % 1001 == 0;
+    }
+    if (num < 10000000) {
+        return false;
+    }
+    if (num < 100000000) {
+        return num % 10001 == 0;
+    }
+    if (num < 1000000000) {
+        return false;
+    }
+    if (num < 10000000000) {
+        return num % 100001 == 0;
+    }
+    if (num < 100000000000) {
+        return false;
+    }
+    // Crash: Need to extend this.
+    abort();
+}
+
 int main(int argc, char* argv[]) {
     if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " <input_file>" << std::endl;
@@ -18,10 +58,42 @@ int main(int argc, char* argv[]) {
     std::cout << "Solving Day 02 Part 1..." << std::endl;
 
     std::string line;
+    uint64_t sumOfBad = 0;
     while (std::getline(input, line)) {
-        // Part 1 logic here
+        int searchStart = 0;
+        while (true) {
+            int nextCommaPosition = line.find(',', searchStart);
+            if (nextCommaPosition == std::string::npos) {
+                break;
+            }
+
+            int dashPosition = line.find('-', searchStart);
+
+            uint64_t firstNum = std::stoull(line.substr(searchStart, dashPosition));
+            uint64_t secondNum = std::stoull(line.substr(dashPosition+1, nextCommaPosition));
+
+            std::cout << firstNum << '-' << secondNum << std::endl;
+    
+
+            // naive approach: just iterate from first to second
+            for (uint64_t i = firstNum; i <= secondNum; i++) {
+                if (isBadId(i)) {
+                    sumOfBad += i;
+                }
+            }
+
+            searchStart = nextCommaPosition + 1;
+        }
     }
 
+    // // test:
+    // for (int i = 0; i < 45; i++) {
+    //     std::cout << i << '\t' << isBadId(i) << std::endl;
+    // }
+
     input.close();
+
+    std::cout << sumOfBad << std::endl;
+
     return 0;
 }
