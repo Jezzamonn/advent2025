475ac05b5963c2abeaa256b50d156ffb73adda84
Jez Swanson <swansonjeremy@google.com>
2025-12-09 10:17:06 -0800

    Day 3, part 1

--- day03/part1.cpp
+++ day03/part1.cpp
@@ -2,6 +2,11 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <cstdint> // For uint8_t and uint32_t
+#include <algorithm> // For std::max
+#include <cmath> // For std::pow
+
+#include "part1.h"
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
@@ -17,11 +22,49 @@ int main(int argc, char* argv[]) {
     
     std::cout << "Solving Day 03 Part 1..." << std::endl;
 
+    uint32_t sum = 0;
     std::string line;
     while (std::getline(input, line)) {
-        // Part 1 logic here
+        uint8_t bestJoltage = getBestJoltages(line, 2)[0];
+        sum += bestJoltage;
+        std::cout << line << '\t' << static_cast<int>(bestJoltage) << std::endl;
     }
 
     input.close();
+
+    std::cout << sum << std::endl;
+
     return 0;
+}
+
+std::vector<uint8_t> getBestJoltages(std::string line, uint8_t numDigits) {
+
+    // start at back, it's just the cumulative max
+    std::vector<uint8_t> bestJoltages(line.length());
+
+    // single digit case.
+    if (numDigits == 1) {
+        uint8_t max = 0;
+        for (int i = line.length() - 1; i >= 0; i--) {
+            uint8_t digit = line[i] - '0';
+            max = std::max(max, digit);
+            bestJoltages[i] = max;
+        }
+    }
+    else {
+        std::vector<uint8_t> prevBestJoltages = getBestJoltages(line, numDigits - 1);
+
+        // This only really works for n = 2 though because of the uint8_t. That's fine.
+        uint8_t powerOfTen = std::pow(10, numDigits - 1);
+
+        uint8_t max = 0;
+        for (int i = line.length() - numDigits; i >= 0; i--) {
+            uint8_t digit = line[i] - '0';
+            uint8_t bestUsingDigit = digit * powerOfTen + prevBestJoltages[i + 1];
+            max = std::max(max, bestUsingDigit);
+            bestJoltages[i] = max;
+        }
+    }
+
+    return bestJoltages;
 }
---
+++ day03/part1.h
@@ -0,0 +1,4 @@
+#include <vector>
+#include <string>
+
+std::vector<uint8_t> getBestJoltages(std::string line, uint8_t numDigits);
